7e05b0f4a291a0795e87ef6ec6e22ef90dc3dc42
zhaoshuo <690515591@qq.com>
2025-09-19 16:58:27 +0800

    添加菜单

---
+++ apps/web-antd/src/router/routes/modules/home.ts
@@ -0,0 +1,28 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+import { $t } from '#/locales';
+
+const routes: RouteRecordRaw[] = [
+  {
+    meta: {
+      icon: 'mdi:home',
+      title: $t('page.home.title'),
+    },
+    name: 'Home',
+    path: '/home',
+    redirect: '/home/index',
+    children: [
+      {
+        name: 'HomeIndex',
+        path: '/home/index',
+        component: () => import('#/views/home/home.vue'),
+        meta: {
+          icon: 'mdi:home',
+          title: $t('page.home.home'),
+        },
+      },
+    ],
+  },
+];
+
+export default routes;
